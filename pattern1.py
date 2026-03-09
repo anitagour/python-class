e1d47fc5beacf6897117c343ece87917136e022a
anitagour <111350399+anitagour@users.noreply.github.com>
2026-03-09 10:16:40 +0000

    final commit

--- pattern1.py
+++ pattern1.py
@@ -1,7 +1,105 @@
 #pattern 
 
-for i in range(3):
-    print("*")
+# for i in range(3):
+#     print("*")
 
-for i in range(3):
-    print("*",end="")
+# for i in range(3):
+#     print("*",end="")
+
+# for i in range(3):
+#     for j in range(3):
+#         print("*",end="")
+#     print()
+
+# for i in range(3):
+#     for j in range(3):
+#         print(i,end="")
+#     print()
+
+# for i in range(3):
+#     for j in range(3):
+#         print(j,end="")
+#     print()
+
+
+# row= int(input("enter row count="))
+# column=int(input("enter column count="))
+# for i in range(row):
+#     for j in range(column):
+#         print("*",end="")
+#     print()
+
+# row= int(input("enter row count="))
+# column=int(input("enter column count="))
+# for i in range(row):
+#     for j in range(column):
+#         if i==j:
+#          print("1",end="")
+#         else:
+#            print("0",end="")
+#     print()
+
+#left-incremental triangle 
+# Pattern
+# for i in range(3):
+#     print("*")
+
+# for i in range(3):
+#     for j in range(3):
+#      print("*",end="")  
+#     print()
+
+# for i in range(3):
+#     for j in range(3):
+#      print(i,end="")  
+#     print()
+
+# for i in range(3):
+#     for j in range(3):
+#      print(j,end="")  
+#     print()
+
+
+# row=int(input("Enter row count="))
+# column=int(input("Enter column count="))
+# for i in range(row):
+#     for j in range(column):
+#         print("*",end="")
+#     print()
+
+# row=int(input("Enter row count="))
+# column=int(input("Enter column count="))
+# for i in range(row):
+#     for j in range(column):
+#         if i==j:
+#             print("1",end="")
+#         else:
+#             print("0",end="")
+#     print()
+
+# Left-incremental Triangle
+count=int(input("Enter row count="))
+for i in range(count):
+    for j in range(i):
+            print("*",end="")
+    print()
+
+count=int(input("Enter row count="))
+for i in range(count):
+    for j in range(i+1):
+            print(i,end="")
+    print()
+
+count=int(input("Enter row count="))
+for i in range(count):
+    for j in range(i+1):
+            print(j,end="")
+    print()
+    
+count=int(input("Enter row count="))
+n=1
+for i in range(count):
+    for j in range(i+1):
+            print(i,end="")
+            n+=1
+    print()
